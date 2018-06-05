035715ebdddfa6c1aac694ce78f7d0c0fe0dece1
Enas Elkhouly <enas.a.elkhouly@gmail.com>
2018-06-05 10:31:19 -0700

    Production configuration file changes

--- configs/enviroments/production.js
+++ configs/enviroments/production.js
@@ -1,12 +1,9 @@
 'use strict';
-let path = require('path');
-
-let rootPath = path.normalize(path.join(__dirname, "..", ".."));
 
 let localConfig = {
     viewEngine: 'ejs',
     secret: "I am so grateful for everything in my life!",
-    rootPath: rootPath
+    rootPath: './'
 };
 
 module.exports = localConfig;
