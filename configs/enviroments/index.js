8ed5bcc01a589a05b56e0c99acd77414ed9f98c0
Enas Elkhouly <enas.a.elkhouly@gmail.com>
2018-06-04 21:41:48 -0700

    Fix building errors

--- configs/enviroments/index.js
+++ configs/enviroments/index.js
@@ -3,11 +3,9 @@
 const
     _ = require('lodash'),
     env = process.env.NODE_ENV || 'local';
-    // const envConfig = require('./' + env);
 
 let defaultConfig = {
     env: env
 };
 
-//module.exports = _.merge(defaultConfig, envConfig);
 module.exports = defaultConfig;
