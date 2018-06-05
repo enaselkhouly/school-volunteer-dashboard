1ed08afdef2fe57e90c0f64ec1fba8fc2ac55541
Enas Elkhouly <enas.a.elkhouly@gmail.com>
2018-06-05 10:58:38 -0700

    Production configuration file changes

--- configs/enviroments/production.js
+++ configs/enviroments/production.js
@@ -4,6 +4,7 @@ let path = require('path');
 let rootPath = path.normalize(path.join(__dirname, "..", ".."));
 
 let localConfig = {
+    hostname: 'sch-volunteer-dashboard.herokuapp.com/'
     viewEngine: 'ejs',
     secret: "I am so grateful for everything in my life!",
     rootPath: 'rootPath'
