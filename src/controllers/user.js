ea859339c694ea815420d9d5f6e50d36ae962f38
Enas Elkhouly <enas.a.elkhouly@gmail.com>
2018-06-06 09:35:41 -0700

    temp remove user register route auth

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -9,7 +9,7 @@ const User = require('../models/User'),
 /* Show the registeration form for new User.*/
 function getRegister ( req, res) {
 
-  let userDir = req.user.userType.toLowerCase();
+  //let userDir = req.user.userType.toLowerCase();
 
   res.render(`user/admin`, {
             title: 'Register',
