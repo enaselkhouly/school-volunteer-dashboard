aff084de3724c0faf403eaf74fb3c76d7ee8f955
Enas Elkhouly <enas.a.elkhouly@gmail.com>
2018-06-29 21:57:02 -0700

    Modified eslintrc configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "jquery": true,
-        "mocha": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaVersion": "6"
-    },
-    "rules": {
-        "no-var": "error",
-        "no-multiple-empty-lines": "warn",
-        "prefer-const": "off",
-        "no-console": "off",
-        "no-empty": "error",
-        "no-unused-vars": [
-          "error"
-        ]
-    }
+"parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": "6"
+},
+"extends": "eslint:recommended",
+"env": {
+    "node": true,
+    "es6": true
+  },
+"rules": {
+    "no-var": "error",
+    "no-multiple-empty-lines": "warn",
+    "prefer-const": "off",
+    "no-console": "off",
+    "no-empty": "error",
+    "no-unused-vars": [
+      "error"
+    ]
+}
 };
--- src/public/.eslintrc.js
+++ src/public/.eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "jquery": true,
-        "node": false
-    },
-    "parserOptions": {
-        "sourceType": "script"
-    },
-    "rules": {
-        "no-var": "off",
-        "prefer-const": "off",
-        "no-undef": "off",
-        "no-unused-vars": [
-          "error",
-          {
-            "args": "none"
-          }
-        ]
-    }
+  "parserOptions": {
+      "sourceType": "script"
+  },
+  "env": {
+      "browser": true,
+      "commonjs": true,
+      "jquery": true,
+      "node": false
+  },
+  "rules": {
+      "no-var": "off",
+      "prefer-const": "off",
+      "no-undef": "off",
+      "no-unused-vars": [
+        "error",
+        {
+          "args": "none"
+        }
+      ]
+  }
 };
