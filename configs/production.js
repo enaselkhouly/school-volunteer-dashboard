6739389a867ce20ac8c501997ff96932a6f38391
Enas Elkhouly <enas.a.elkhouly@gmail.com>
2018-07-01 13:33:17 -0700

    Fixing db url error in production

--- configs/production.js
+++ configs/production.js
@@ -1,5 +1,14 @@
 "use strict";
 
+let pkg 			 = require("../package.json");
+
 module.exports = {
+  app: {
+    title: pkg.name + " [Development mode]"
+  },
+
+  db: {
+    url: process.env.MONGO_URI || "mongodb://localhost/" + pkg.config.dbName + "-production",
+  },
 
 };
