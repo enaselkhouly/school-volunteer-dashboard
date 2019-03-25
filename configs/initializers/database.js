83e391624a11648e35ee99099f2f1feddbd4c268
Enas Elkhouly <enas.a.elkhouly@gmail.com>
2019-03-25 13:30:24 -0700

    Fix build warnings

--- configs/initializers/database.js
+++ configs/initializers/database.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 function connect (config) {
 
 		mongoose.Promise = require('bluebird');
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true);
+
 		const dbURL = process.env.DATABASEURL || config.db.url;
 		mongoose.connect(dbURL);
 		console.log('Database is connected on: ', dbURL);
